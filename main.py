0f5e9db4fb324053a25e08565a7e7e94b6f7f6a2
JuuuuuJu <y5c0901720@gmail.com>
2026-01-19 17:33:33 +0800

    add CANCEL and cmd_id

--- main.py
+++ main.py
@@ -2,58 +2,57 @@ import argparse
 from bt_controller import ESP32BTSender
 import time
 
-# 設定 Port (請修改為實際 Port)
 PORT = 'COM3' 
 
 def main():
     try:
         with ESP32BTSender(port=PORT) as sender:
+            success = sender.send_burst(
+                cmd_input='PLAY',
+                delay_sec=2, 
+                prep_led_sec=1,
+                target_ids=[0, 1, 5],
+                data=[0, 0, 0], # CANCEL: data[0]=cmd_id / TEST: rgb
+                retries=3,
+            )
+            time.sleep(3)
+            success = sender.send_burst(
+                cmd_input='PAUSE',
+                delay_sec=5, 
+                prep_led_sec=1,
+                target_ids=[0, 1, 5],
+                data=[0, 0, 0],
+                retries=3,
+            )
+            time.sleep(3)
+            success = sender.send_burst(
+                cmd_input='CANCEL',
+                delay_sec=1, 
+                prep_led_sec=1,
+                target_ids=[0, 1, 5],
+                data=[1, 0, 0],
+                retries=3,
+            )
+            time.sleep(3)
+            success = sender.send_burst(
+                cmd_input='TEST',
+                delay_sec=7, 
+                prep_led_sec=1,
+                target_ids=[0, 1, 5],
+                data=[0, 0, 255],
+                retries=3,
+            )
+            time.sleep(3)
             # success = sender.send_burst(
-            #     cmd_input='PLAY',
-            #     delay_sec=4, 
+            #     cmd_input='',
+            #     delay_sec=5, 
             #     prep_led_sec=1,
             #     target_ids=[0, 1, 5],
-            #     r=0,
-            #     g=0,
-            #     b=0,
+            #     data=[0, 0, 0],
             #     retries=3,
             # )
             # time.sleep(3)
             
-            # success = sender.send_burst(
-            #     cmd_input='TEST',
-            #     delay_sec=6, 
-            #     prep_led_sec=1,
-            #     target_ids=[0, 1, 5],
-            #     r=0,
-            #     g=255,
-            #     b=0,
-            #     retries=3,
-            # )
-            # time.sleep(3)
-
-            # success = sender.send_burst(
-            #     cmd_input='RESET',
-            #     delay_sec=7, 
-            #     prep_led_sec=1,
-            #     target_ids=[0, 1, 5],
-            #     r=0,
-            #     g=0,
-            #     b=0,
-            #     retries=3,
-            # )
-            # time.sleep(3)
-            success = sender.send_burst(
-                cmd_input='LOAD',
-                delay_sec=8, 
-                prep_led_sec=1,
-                target_ids=[0, 1, 5],
-                r=0,
-                g=0,
-                b=0,
-                retries=3,
-            )
-            time.sleep(3)
     except Exception as e:
         print(f"Main execution error: {e}")
 
